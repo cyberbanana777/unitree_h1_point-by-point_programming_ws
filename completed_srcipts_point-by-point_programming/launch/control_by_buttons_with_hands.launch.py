648561a003dc0eeb0495f0190eafdcd97bdfee49
Alexander Grachev <sashagrachev2005@gmail.com>
2025-08-12 18:45:32 +0300

    Commit on 2025-08-12_18:45:32

---
+++ completed_srcipts_point-by-point_programming/launch/control_by_buttons_with_hands.launch.py
@@ -0,0 +1,21 @@
+from launch_ros.actions import Node
+
+from launch import LaunchDescription
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="buttons_server",
+                executable="server_stand_up_with_hands",
+            ),
+
+            Node(package="buttons_server", executable="hear_server"),
+            
+            Node(
+                package="button_analyzer",
+                executable="button_analyzer_with_hands",
+            ),
+        ]
+    )
